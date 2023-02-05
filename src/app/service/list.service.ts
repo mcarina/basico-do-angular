b9b42c7f051b1b88c53a6f3f089db8f58da95faa
Márcia Carina <102268481+mcarina@users.noreply.github.com>
2023-02-05 18:03:58 -0400

    Add files via upload

--- src/app/service/list.service.ts
+++ src/app/service/list.service.ts
@@ -7,14 +7,22 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ListService {
+  // Banco de dados vindo de outro lugar
   private apiUrl = 'http://localhost:3000/animals'
 
   constructor(private http: HttpClient ) { }
 
-  remove(animals: Animal[], animal: Animal){
-    console.log('ativando service')
-    return animals.filter((a) => animal.name !== a.name)
-  }
+  // Trabalha em conjunto com o list-render e excluir no back-end
+  remove(id: number){
+    return this.http.delete<Animal>(`${this.apiUrl}/${id}`)
+   }
+
+
+  //PRIMEIRO METODO SEM "EXCLUIR DE FATO"
+  //remove(animals: Animal[], animal: Animal){
+    // console.log('ativando service')
+    // return animals.filter((a) => animal.name !== a.name)
+//}
 
   getAll(): Observable<Animal[]>{
     return this.http.get<Animal[]>(this.apiUrl)
