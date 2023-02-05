d425f0d2aa88a0fa41bb644616e4011b4097232a
Márcia Carina <102268481+mcarina@users.noreply.github.com>
2023-02-05 18:04:26 -0400

    Add files via upload

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -19,15 +19,23 @@ export class ListRenderComponent {
     this.getAnimals()
   }
 
+  // Mostra a frase em baixo após apertar o botão
   ageAnimal(animal: Animal): void{
     this.animalDetais = ` ${animal.name} tem ${animal.age} anos`
   }
-
+  
+  // botão de excluir um animal da lista
   removeAnimal(animal: Animal){
-    console.log('removendo animals');
-    this.animal = this.listService.remove(this.animal, animal);
+    //SEGUNDO METODO de exclução no front-ends
+    this.animal = this.animal.filter((a) => animal.name !== a.name)
+    this.listService.remove(animal.id).subscribe();
+    //PRIMEIRO METODO EM CONJUNTO NO 'SERVICE' SEM EXCLUIR DE FATO
+    // console.log('removendo animals');
+    // this.animal = this.listService.remove(this.animal, animal);
   }
 
+
+  // pega as informações dos animais pra outra page
   getAnimals(): void {
     this.listService.getAll().subscribe((animals)=>(this.animal=animals))
   }
